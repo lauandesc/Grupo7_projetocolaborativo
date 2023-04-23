104ab0b1996b72e8c4831c04c820b7035b5efdfe
flaviocmb <flavio.cmb@gmail.com>
2023-04-23 19:16:11 -0300

    desenvolvimento do script

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 const botoesQuantidade = document.querySelectorAll('.botoesquantidade');
 
+// Pega todos os links "Remover este item"
+const linksRemover = document.querySelectorAll('.removerlista');
+
+// Itera pelos links e adiciona um evento de clique em cada um
+for (let i = 0; i < linksRemover.length; i++) {
+  linksRemover[i].addEventListener('click', function(event) {
+    // Impede o comportamento padrão do link
+    event.preventDefault();
+    // Remove o elemento pai do link, ou seja, a div class gridcarrinho
+    this.parentElement.parentElement.parentElement.remove();
+  });
+}
+
 botoesQuantidade.forEach(botaoQuantidade => {
   const btnMenos = botaoQuantidade.querySelector('.btn-menos');
   const btnMais = botaoQuantidade.querySelector('.btn-mais');
