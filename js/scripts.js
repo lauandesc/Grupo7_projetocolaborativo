f2003f3aa33e15a15307b1dbc07e0f38ba6d91e3
flaviocmb <flavio.cmb@gmail.com>
2023-04-23 03:48:54 -0300

    desenvolvimento e alterações.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
-const inputQuantidade = document.querySelector('.botoesquantidadeb input.quantidade');
-const btnMenos = document.querySelector('.botoesquantidadeb a.btn-menos');
-const btnMais = document.querySelector('.botoesquantidadeb a.btn-mais');
+const botoesQuantidade = document.querySelectorAll('.botoesquantidade');
 
-// adiciona o evento de clique ao botão de menos
-btnMenos.addEventListener('click', function(event) {
-  event.preventDefault();
-  if (inputQuantidade.value > 1) {
-    inputQuantidade.value--;
-  }
-});
+botoesQuantidade.forEach(botaoQuantidade => {
+  const btnMenos = botaoQuantidade.querySelector('.btn-menos');
+  const btnMais = botaoQuantidade.querySelector('.btn-mais');
+  const inputQuantidade = botaoQuantidade.querySelector('.quantidade');
 
-// adiciona o evento de clique ao botão de mais
-btnMais.addEventListener('click', function(event) {
-  event.preventDefault();
-  inputQuantidade.value++;
-});
+  btnMenos.addEventListener('click', () => {
+    if (inputQuantidade.value > 1) {
+      inputQuantidade.value--;
+    }
+  });
 
-// adiciona o evento de input ao input de quantidade
-inputQuantidade.addEventListener('input', function(event) {
-  const newValue = parseInt(event.target.value);
-  if (isNaN(newValue) || newValue < event.target.min) {
-    event.target.value = inputQuantidade.value;
-  }
+  btnMais.addEventListener('click', () => {
+    inputQuantidade.value++;
+  });
+
+  inputQuantidade.addEventListener('change', () => {
+    if (inputQuantidade.value < 1) {
+      inputQuantidade.value = 1;
+    }
+  });
 });
