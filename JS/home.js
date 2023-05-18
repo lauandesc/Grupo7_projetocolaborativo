68b07a05eba35753ef5e9c3219ce1570e4e19236
flaviocmb <flavio.cmb@gmail.com>
2023-05-18 17:43:31 -0300

    configuração inicial

---
+++ JS/home.js
@@ -0,0 +1,17 @@
+let radio = document.querySelector('.manual-btn')
+let cont = 1
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 2000)
+
+function proximaImg(){
+    cont++
+    if(cont > 4){
+        cont = 1
+    }
+
+    document.getElementById('radio' +cont).checked = true
+
+}
