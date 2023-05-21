a7fc1d986f8fbfede202a93b01ba4f8e3ebca369
lauandesc <lauandes1987@gmail.com>
2023-05-21 12:44:03 -0300

    slide pronto

--- JS/home.js
+++ JS/home.js
@@ -1,17 +1,17 @@
-let radio = document.querySelector('.manual-btn')
-let cont = 1
-document.getElementById('radio1').checked = true
+// let radio = document.querySelector('.manual-btn')
+// let cont = 1
+// document.getElementById('radio1').checked = true
 
-setInterval(() => {
-    proximaImg()
-}, 2000)
+// setInterval(() => {
+//     proximaImg()
+// }, 2000)
 
-function proximaImg(){
-    cont++
-    if(cont > 4){
-        cont = 1
-    }
+// function proximaImg(){
+//     cont++
+//     if(cont > 4){
+//         cont = 1
+//     }
 
-    document.getElementById('radio' +cont).checked = true
+//     document.getElementById('radio' +cont).checked = true
 
-}
+// }
