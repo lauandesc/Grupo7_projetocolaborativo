c7ef390f903dbbaa5e9edf4e8173705d061118e9
Rogerio <rmbmora@gmail.com>
2023-06-14 15:35:22 -0300

    sql consultas atualizado

--- sql/comandos.sql
+++ sql/comandos.sql
@@ -212,12 +212,18 @@ INNER JOIN item_carrinho ON carrinho.id = item_carrinho.fk_id_carrinho
 INNER JOIN livro ON item_carrinho.fk_id_livro = livro.id
 WHERE usuario.nome_completo = 'Pedro Santos';
 
-SELECT usuario.nome_completo, doacao_livro.tipo_doacao, doacao_livro.conservacao, doacao_livro.titulo, doacao_livro.autor, doacao_livro.editora, doacao_livro.quantidade
+SELECT usuario.nome_completo, doacao_livro.tipo_doacao, doacao_livro.conservacao, doacao_livro.titulo, doacao_livro.autor, doacao_livro.editora, doacao_livro.quantidade, doacao_livro.img_conservacao
 FROM usuario 
 INNER JOIN doacao_livro ON usuario.id = doacao_livro.fk_id_usuario
 WHERE tipo_doacao = 'Revista';
 
-SELECT usuario.nome_completo, doacao_livro.tipo_doacao, doacao_livro.conservacao, doacao_livro.titulo, doacao_livro.autor, doacao_livro.editora, doacao_livro.quantidade
+SELECT usuario.nome_completo, doacao_livro.tipo_doacao, doacao_livro.conservacao, doacao_livro.titulo, doacao_livro.autor, doacao_livro.editora, doacao_livro.quantidade, doacao_livro.img_conservacao
 FROM usuario 
 INNER JOIN doacao_livro ON usuario.id = doacao_livro.fk_id_usuario
 WHERE tipo_doacao = 'Revista' AND doacao_livro.conservacao = 'Novo';
+
+SELECT usuario.nome_completo, doacao_livro.tipo_doacao, doacao_livro.conservacao, doacao_livro.titulo, doacao_livro.autor, doacao_livro.editora, doacao_livro.quantidade, doacao_livro.img_conservacao
+FROM usuario 
+INNER JOIN doacao_livro ON usuario.id = doacao_livro.fk_id_usuario
+WHERE quantidade >3;
+
